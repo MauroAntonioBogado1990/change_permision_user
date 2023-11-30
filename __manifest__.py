42ab6474fd32b918925c1c0b3d557977ee5db204
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2023-11-30 17:31:27 -0300

    add models and views permisions

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Change Of Inventory In Sales',
+    'version': '16.0',
+    'depends': [
+        'base','product','sale','stock',
+    ],
+    'author': 'Mauro Bogado, Exemax',
+    'website': '"http://www.exemax.com.ar"',
+    'summary': 'Modulo para cambiar visualizacion de compras y ventas',
+    'category': 'Extra Tools',
+    'description': '''
+   Modulo para cambiar visualizacion de compras y ventas.
+    ''',
+    'data': [
+        'views/change_permision_user.xml',      
+    
+        
+       
+    ],
+}
---
+++ models/change_permision_user.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+
+class ChangePermisionUser(models.Model):
+    _inherit = 'product.template'
